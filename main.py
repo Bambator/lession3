990dd23af87c957689325e91c797a25c090faac6
Bambator <130966909+Bambator@users.noreply.github.com>
2024-10-04 22:25:48 +0300

    add all const

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import paygame
-import pygame.display
+import pygame
+import random
 
 paygame.init()
 
@@ -8,7 +8,15 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode(SCREEN_WIDTH,SCREEN_HEIGHT)
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("Img/screen_icon.jpg")
+
+target_img = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
+target_x = randome.randim(0, SCREEN_WIDTH - target_width)
+target_y = randome.randim(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
